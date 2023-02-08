114e63a7cb9b0047f1b8072f6bc8a645e0d81233
unknown <htoanghostsniper@gmail.com>
2023-02-08 23:02:34 +0700

    add more implement on MotorDrv

--- src/MotorDrv.h
+++ src/MotorDrv.h
@@ -2,9 +2,45 @@
 #define MOTORDRV_H
 /////////////////////////
 
-enum E_MotorState{STOP_STATE, RUN_STATE, ERROR_STATE};
+enum E_MotorState {
+    STOP_STATE, 
+    RUN_STATE, 
+    ERROR_STATE
+};
 
+enum E_StartSequence {
+    NO_START_SEQUENCE,
+    START_SEQUENCE_ENBACKLIGHT,
+    START_SEQUENCE_MS1MS2DIR,
+    START_SEQUENCE_ENBL,
+    START_SEQUENCE_NSLEEP
+};
 
+enum E_StopSequence {
+    NO_STOP_SEQUENCE,
+    STOP_SEQUENCE_NSLEEP,
+    STOP_SEQUENCE_ENBL,
+    STOP_SEQUENCE_MS1MS2DIR,
+    STOP_SEQUENCE_ENBACKLIGHT
+};
+
+enum E_ResetSequence {
+    NO_RESET_SEQUENCE,
+    RESET_START_SEQUENCE_ENBACKLIGHT,
+    RESET_START_SEQUENCE_MS1MS2DIR,
+    RESET_START_SEQUENCE_ENBL,
+    RESET_START_SEQUENCE_NSLEEP
+    RESET_STOP_SEQUENCE_NSLEEP = 101,
+    RESET_STOP_SEQUENCE_ENBL = 102,
+    RESET_STOP_SEQUENCE_MS1MS2DIR = 103,
+    RESET_STOP_SEQUENCE_ENBACKLIGHT = 104
+};
+
+int MotorDrv_GetState();
+void MotorDrv_SetState(int motorState);
+void MotorDrv_StartSequence();
+void MotorDrv_StopSequence();
+void MotorDrv_ResetSequence();
 
 /////////////////////////
 #endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 #include "stdio.h"
 #include "MotorDrv.h"
 int main(){
-    int a = 2;
-    a = add(a);
-    printf("%d hello",get());
     return 0;
 }
